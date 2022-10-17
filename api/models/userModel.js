6dda8b1285a2077203e5b537d740cf86ac432e60
Maor <maorgabaynew@gmail.com>
2022-10-17 17:25:09 +0300

    change the ui

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
 let userSchema = new mongoose.Schema({
-  name:String,
-  email:String,
-  password:String,
-  date_created:{
-    type:Date , default:Date.now()
+  fullName: {
+    firstName: String,
+    lastName: String
+  },
+  email: String,
+  password: String,
+  date_created: {
+    type: Date, default: Date.now()
   },
   // role of the user if regular user or admin
-  role:{
-    type:String, default:"user"
+  role: {
+    type: String, default: "user"
   }
 })
 
-exports.UserModel = mongoose.model("users",userSchema);
+exports.UserModel = mongoose.model("users", userSchema);
--- api/validation/userValidation.js
+++ api/validation/userValidation.js
@@ -3,7 +3,10 @@ const Joi = require("joi");
 
 exports.validUser = (_reqBody) => {
     let joiSchema = Joi.object({
-      name:Joi.string().min(2).max(99).required(),
+      fullName: {
+      firstName: Joi.string().min(2).max(50).required(),
+      lastName: Joi.string().min(2).max(50).required(),
+  },
       email:Joi.string().min(2).max(99).email().required(),
       password:Joi.string().min(3).max(99).required()
     })
